4e616786f263672dbc74b944e7707bdcbefc70c7
snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-08-02 22:28:20 -0600

    Hardware config moved into repo (for now)
    Signed-off-by: snoutibur <157987813+snoutibur@users.noreply.github.com>

--- hosts/hp-aio/config.nix
+++ hosts/hp-aio/config.nix
@@ -15,14 +15,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05";
+  system.stateVersion = "25.05"; # Initial NixOS install version. Recommended to leave alone.
 
 
   # Packages #
