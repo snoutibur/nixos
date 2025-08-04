94f31d3dde8b294d6695175ceb9c38a62b1f2478
snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-08-03 23:07:38 -0600

    Hypr packages additions
    Signed-off-by: snoutibur <157987813+snoutibur@users.noreply.github.com>

--- modules/packages/hypr.nix
+++ modules/packages/hypr.nix
@@ -7,9 +7,11 @@
   
   config = lib.mkIf config.hypr.enable {
     programs.hyprland.enable = true;
+
     environment.systemPackages = with pkgs; [
       brightnessctl # Display brightness
-      swaybg # background setter
+      hyprpaper # background setter
+      hyprsunset # night shift
       pamixer # Media controls
 
       grim # CLI scrnshot tool
@@ -17,8 +19,17 @@
 
       mako # notifs
       waybar # status bar
+      eww # widgets
       wofi # launcher
       wl-clipboard # clipboard functionality
+
+      hyprlock # lock screen
+      hypridle # idle daemon
+
+      hyprland-qt-support # patches
+      hyprland-qtutils # additional QT patches
+      xdg-desktop-portal-hyprland # comm to compositor thru D-Bus
+      hyprpolkitagent # Needed for GUI apps to request elevated perms
     ];
 };
 }
