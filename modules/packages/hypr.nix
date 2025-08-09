165b1855b3ee78482d573656b791fc15915f288b
snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-08-09 10:50:45 -0600

    Organized hypr.nix
    Signed-off-by: snoutibur <157987813+snoutibur@users.noreply.github.com>

--- modules/packages/hypr.nix
+++ modules/packages/hypr.nix
@@ -8,13 +8,11 @@
   config = lib.mkIf config.hypr.enable {
     programs.hyprland.enable = true;
 
+    xdg.portal.enable = true;
+
     environment.systemPackages = with pkgs; [
       ## Apps & Window management
       wofi # launcher
-
-      ## System control ##
-      brightnessctl # Display brightness
-      hyprsunset # night shift
 
       ## DE items ##
       hyprlock # lock screen
@@ -27,17 +25,26 @@
       pamixer # Media controls
       eww # widgets
 
+
       ## Utils ##
       grim # CLI scrnshot tool
       slurp # scrnshot regions
       swappy # Image editor/annotator
+
+
+      ## System control ##
+      brightnessctl # Display brightness
+      hyprsunset # night shift
+
 
       ## Libs ##
       hyprland-qt-support # patches
       hyprland-qtutils # additional QT patches
       hyprpolkitagent # Needed for GUI apps to request elevated perms
       xdg-desktop-portal-hyprland # comm to compositor thru D-Bus
-      libnotify
+      xdg-desktop-portal-gtk # additional desktop interactions
+      xwayland # bridge x11 -> wayland
+      libnotify # notif handler
     ];
 };
 }
