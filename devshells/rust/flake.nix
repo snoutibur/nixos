020450012d7c35affc427c9adf0d938b9e1020a2
snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-08-13 22:49:43 -0600

    Rustup based rust dev
    Signed-off-by: snoutibur <157987813+snoutibur@users.noreply.github.com>

--- devshells/rust/flake.nix
+++ devshells/rust/flake.nix
@@ -2,23 +2,28 @@
   description = "Rust dev env";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        pkgs-unstable = import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-			cargo
-			rustc
-			rustup
-
-			jetbrains.rust-rover
+            rustup
+            pkgs-unstable.jetbrains.rust-rover
           ];
 
           shellHook = ''
@@ -26,5 +31,6 @@
             echo "rustup default stable to install toolchain"
           '';
         };
-      });
+      }
+    );
 }
