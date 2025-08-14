d4daa1bd57cda3cdaf843ca8602bd5cc9ea0eb9b
snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-08-13 21:35:25 -0600

    Rust tooling
    Signed-off-by: snoutibur <157987813+snoutibur@users.noreply.github.com>

--- modules/packages/devtools.nix
+++ modules/packages/devtools.nix
@@ -10,7 +10,15 @@
         kdePackages.kate # basic text editor
         trufflehog # secrets scanner
         git-filter-repo # rewriting git history
+
+        # Rust development
+        pkgs-unstable.cargo
+        pkgs-unstable.rustc
+
+        # Jetbrains tooling
         pkgs-unstable.jetbrains.idea-ultimate
+        pkgs-unstable.jetbrains.rust-rover
+        pkgs-unstable.jetbrains-toolbox
     ];
   };
 }
