703ef6ca06df551afa89822ce95cfd62e778fd51
snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-12-10 23:39:37 -0700

    More bloatware!
    Signed-off-by: snoutibur <157987813+snoutibur@users.noreply.github.com>

--- modules/packages/bloatware.nix
+++ modules/packages/bloatware.nix
@@ -7,13 +7,16 @@
 
   config = lib.mkIf config.bloat.enable {
     environment.systemPackages = with pkgs; [
+        cmatrix # cool haxxer
         cava # audio visualizer
         cbonsai # trees
         cowsay # Ascii cow & more :33
+        asciiquarium
         figlet # Text to big ascii "art" text
         fortune # fortune...
         peaclock # TUI clock
         pipes # PIIIPES
+        activate-linux # capitalist aaa watermark
     ];
   };
 }
