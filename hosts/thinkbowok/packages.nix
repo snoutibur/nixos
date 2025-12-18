b13f1154086a0a750d375ccb64fc15d55ec70cf8
snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-12-17 17:07:29 -0700

    Laptop packages update
    Signed-off-by: snoutibur <157987813+snoutibur@users.noreply.github.com>

--- hosts/thinkbowok/packages.nix
+++ hosts/thinkbowok/packages.nix
@@ -4,12 +4,14 @@
   # Enable package sets for this machine
   daily.enable = true;
   productivity.enable = true;
+  games.enable = true;
   bloat.enable = true;
 
+  # Development
   devtools.enable = true;
   cdev.enable = false;
   java.enable = true;
   js.enable = true;
-  pydev.enable = true;
-  rust.enable = true;
+  pydev.enable = false;
+  rust.enable = false;
 }
