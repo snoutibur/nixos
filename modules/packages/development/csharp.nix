126a626bbbef91f61e21fac3e0b0df6f935f0550
snoutibur <157987813+snoutibur@users.noreply.github.com>
2026-01-07 10:49:47 -0700

    Fixed C# .NET environment

--- modules/packages/development/csharp.nix
+++ modules/packages/development/csharp.nix
@@ -8,7 +8,8 @@
   config = lib.mkIf config.csdev.enable {
     environment.systemPackages = with pkgs; [
       # language toolchain
-
+      dotnet-sdk_10
+      dotnet-runtime_10
 
       # tooling
       pkgs-unstable.jetbrains.rider
